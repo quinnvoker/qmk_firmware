de0deb0a82ccb0de82c713892bd0a00f6863f8d5
Quinn <quinnvoker@gmail.com>
2021-05-03 13:39:36 -0700

    Fix numpad numbers and media keys

--- keyboards/crkbd/rev1/common/keymaps/quinnvoker/rules.mk
+++ keyboards/crkbd/rev1/common/keymaps/quinnvoker/rules.mk
@@ -2,3 +2,4 @@ MOUSEKEY_ENABLE     = no     # Mouse keys
 RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
 OLED_DRIVER_ENABLE  = yes
 LTO_ENABLE          = yes
+EXTRAKEY_ENABLE     = yes
