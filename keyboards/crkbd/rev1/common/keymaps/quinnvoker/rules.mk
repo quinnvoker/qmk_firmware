9faeb952464abdcae10dace67fd2e2195b5d72de
Quinn <quinnvoker@gmail.com>
2021-07-26 14:09:02 -0700

    Use dfu bootloader

--- keyboards/crkbd/rev1/common/keymaps/quinnvoker/rules.mk
+++ keyboards/crkbd/rev1/common/keymaps/quinnvoker/rules.mk
@@ -3,3 +3,4 @@ RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
 OLED_DRIVER_ENABLE  = yes
 LTO_ENABLE          = yes
 EXTRAKEY_ENABLE     = yes
+BOOTLOADER					=	qmk-dfu # set up for elite c
